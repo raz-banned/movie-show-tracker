22100b7a7805e36f4ff6321f25a79a758fde6130
Ansar Serikov <purplestick553@gmail.com>
2026-06-22 11:41:13 +0500

    feat(api): create baseurl for fetch api

--- src/api/apiOptions.ts
+++
@@ -1,7 +0,0 @@
-export const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-  },
-}
---
+++ src/lib/api.ts
@@ -0,0 +1,17 @@
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+  },
+}
+
+const createApi = (baseUrl: string) => {
+  return (endpoint: string, options: RequestInit = {}) => {
+    return fetch(`${baseUrl}${endpoint}`, options)
+  }
+}
+
+export const api = createApi(
+  import.meta.env.VITE_API_URL || "https://api.themoviedb.org/3"
+)
