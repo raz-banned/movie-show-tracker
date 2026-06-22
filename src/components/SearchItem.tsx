17c0f456b376ff70f6095a5fd3c5b18f63772b7b
Ansar Serikov <purplestick553@gmail.com>
2026-06-22 13:10:49 +0500

    feat: add genre filtering inside an item and have genres be responsive

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -2,43 +2,54 @@ import { DotIcon, StarIcon } from "@phosphor-icons/react"
 import { Badge } from "./ui/badge"
 import type { Genres } from "@/types/movie"
 import type { NormalizedMedia } from "@/types/NormalizedMedia"
+import { findGenres } from "@/utils/findGenres"
 
-export function SearchItem({
-  item,
-  genres,
-}: {
-  item: NormalizedMedia
+interface SearchItemProps {
+  movie: NormalizedMedia
   genres: Genres[]
-}) {
+}
+
+export function SearchItem({ movie, genres }: SearchItemProps) {
+  const filteredGenres = findGenres(movie, genres)
+
   return (
     <div className="flex w-full items-center justify-between gap-3">
       <div className="flex items-center gap-3">
-        {item.posterPath ? (
+        {movie.posterPath ? (
           <img
-            src={`https://image.tmdb.org/t/p/w92/${item.posterPath}`}
+            src={`https://image.tmdb.org/t/p/w92/${movie.posterPath}`}
             className="h-14 w-10 rounded-sm object-cover"
           />
         ) : (
           <div className="h-14 w-10 rounded-sm bg-muted" />
         )}
         <div className="flex flex-col gap-1">
-          <span className="text-lg">{item.title}</span>
+          <span className="text-lg">{movie.title}</span>
           <div className="flex items-center">
             <span className="text-sm text-muted-foreground">
-              {item.releaseDate
-                ? new Date(item.releaseDate).getFullYear()
+              {movie.releaseDate
+                ? new Date(movie.releaseDate).getFullYear()
                 : "N/A"}
             </span>
             <DotIcon size={18} />
-            {genres.map((genre) => (
-              <Badge
-                key={genre.id}
-                variant="outline"
-                className="text-xs font-semibold"
-              >
-                {genre.name}
-              </Badge>
-            ))}
+            <Badge
+              key={filteredGenres[0]?.id}
+              variant="outline"
+              className="text-xs font-semibold"
+            >
+              {filteredGenres[0]?.name}
+            </Badge>
+            <div className="hidden flex-wrap gap-1 lg:flex">
+              {filteredGenres.map((genre) => (
+                <Badge
+                  key={genre.id}
+                  variant="outline"
+                  className="text-xs font-semibold"
+                >
+                  {genre.name}
+                </Badge>
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -46,7 +57,7 @@ export function SearchItem({
       <div>
         <Badge className="p-3 text-xs text-[oklch(from_var(--color-primary)_0.8_0.20_h)]">
           <StarIcon size={18} data-icon="inline-start" />
-          {item.voteAverage.toFixed(1)}
+          {movie.voteAverage.toFixed(1)}
         </Badge>
       </div>
     </div>
